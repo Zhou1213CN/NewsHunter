823a47b41542d6f2ddab7e1f77fd584bbb589fbb
Zheng <zhoupt@bc.edu>
2021-05-14 19:40:08 +0800

    Initial Commit

---
+++ NewsHunter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsHunter
+//
+//  Created by Zheng Zhou on 2021/5/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
